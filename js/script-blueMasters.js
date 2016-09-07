f6e0fdf38045d5b4663b0bc5d28509c6d2338f8a
Alexander <buralex89@gmail.com>
2016-09-07 21:34:27 +0000

    sticky footer v.1  !!!

--- js/script-blueMasters.js
+++ js/script-blueMasters.js
@@ -60,32 +60,18 @@ function setEqualHeight(columns) {
   
 ----------------------------------------------------------------------------- */
 
-function stickyFooter(footerContainer) {
+function stickyFooter(footerContainer, wrapCont) {
     function stick() {
-        var bodyHeight = parseInt(getComputedStyle(document.querySelector('body')).height);
-        var windHeight = Math.max( document.documentElement.clientHeight, window.innerHeight || 0 );
-
-       /* if ( bodyHeight < windHeight) {
-           document.querySelector(footerContainer).style.top = (windHeight - bodyHeight) + 'px'; 
- 
-        } else {
-           document.querySelector(footerContainer).style.top = 0 + 'px';
-          }*/
-    
-        var move = windHeight - bodyHeight;
-        if (move > 0) {
-            document.querySelector(footerContainer).style.top = move + 'px';
-            
-        } else {
-            document.querySelector(footerContainer).style.top = 0 + 'px';
-        }
+        
+        var windHeight = Math.max( document.documentElement.clientHeight, window.innerHeight );
+        var footerHeight = parseInt(getComputedStyle(document.querySelector(footerContainer)).height);
+       
+        var expand = windHeight - footerHeight;
+        document.querySelector(wrapCont).style.minHeight = expand + 'px';
     }
 
-   
-   //window.addEventListener("load", stick);
-   stick();
-   //window.addEventListener('resize',  stick);
-   window.addEventListener('resize', debounce(function() { stick() }, 400));
+   debounce(function() { stick() }, 250)();
+   window.addEventListener('resize', debounce(function() { stick() }, 250));
    
 }
 
